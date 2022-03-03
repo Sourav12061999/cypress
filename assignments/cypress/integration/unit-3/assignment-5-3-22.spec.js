222c47f6db2ad65a4bfbb8e260d704bf2871597b
Sourav Das <sdunity12061999@gmail.com>
2022-03-03 10:20:46 +0530

    3 test cases written

---
+++ assignments/cypress/integration/unit-3/assignment-5-3-22.spec.js
@@ -0,0 +1,19 @@
+describe("This the assignment of 5-3-22", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io/todo");
+  });
+
+  it("All the buttons with right ID are there", () => {
+    cy.get("#scope");
+    cy.get("#hoisting");
+    cy.get("#constructor-functions");
+    cy.get("prototype");
+  });
+
+  it("All Local Storage Data is Present", () => {
+    expect(localStorage.getItem("scope")).to.not.equal(null);
+    expect(localStorage.getItem("hoisting")).to.not.equal(null);
+    expect(localStorage.getItem("constructor-functions")).to.not.equal(null);
+    expect(localStorage.getItem("prototype")).to.not.equal(null);
+  });
+});
